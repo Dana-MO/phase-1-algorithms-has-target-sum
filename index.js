238854efc290044d4de42847ad822077b6095378
danaomar <danaomaark@gmail.com>
2023-10-13 11:43:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    const difference = target - array[i];
+    // console.log(difference);
+
+    const newArr = array.filter(notElement)
+   
+    function notElement(number) {
+      if (number !== array[i]) {
+        return number;
+      }
+    }
+
+    if (newArr.indexOf(difference) !== -1) {
+      return true;
+    }
+  }
+
+  return false;
+
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -29,6 +50,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 7, 9, 20], 16));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, -12, 7], 4));
 }
 
 module.exports = hasTargetSum;
